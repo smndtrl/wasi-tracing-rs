d10b3a35734ac67305e24c898466c47475ae8d8f
smndtrl <smndtrl@users.noreply.github.com>
2024-06-05 06:08:18 +0000

    upd

--- src/lib.rs
+++ src/lib.rs
@@ -4,41 +4,33 @@ pub mod bindings {
         interfaces: "
             import wasi:tracing/producer;
         ",
-        tracing: false,
-        async: false,
-        with: {
-            // "wasi:io/error": wasmtime_wasi::preview2::bindings::io::error,
-            // "wasi:io/streams": wasmtime_wasi::preview2::bindings::io::streams,
-            // "wasi:io/poll": wasmtime_wasi::preview2::bindings::io::poll,
-
-            // "wasi:http/types/outgoing-body": super::body::HostOutgoingBody,
-            // "wasi:http/types/future-incoming-response": super::types::HostFutureIncomingResponse,
-            // "wasi:http/types/outgoing-response": super::types::HostOutgoingResponse,
-            // "wasi:http/types/future-trailers": super::body::HostFutureTrailers,
-            // "wasi:http/types/incoming-body": super::body::HostIncomingBody,
-            // "wasi:http/types/incoming-response": super::types::HostIncomingResponse,
-            // "wasi:http/types/response-outparam": super::types::HostResponseOutparam,
-            // "wasi:http/types/outgoing-request": super::types::HostOutgoingRequest,
-            // "wasi:http/types/incoming-request": super::types::HostIncomingRequest,
-            // "wasi:http/types/fields": super::types::HostFields,
-            // "wasi:http/types/request-options": super::types::HostRequestOptions,
-        }
+        // tracing: true,
+        // async: false,
+        trappable_imports: true
     });
 
     pub use wasi::tracing;
 }
 
+fn type_annotate_tracing<T, F>(val: F) -> F
+where
+    F: Fn(&mut T) -> &mut dyn WasiTracingView,
+{
+    val
+}
+
 pub fn add_tracing_to_linker<T>(l: &mut wasmtime::component::Linker<T>) -> anyhow::Result<()>
 where
-    T: WasiTracingView + bindings::wasi::tracing::producer::Host,
+    T: WasiTracingView //+  bindings::wasi::tracing::producer::Host,
 {
-    bindings::tracing::producer::add_to_linker(l, |t| t)?;
+    let closure = type_annotate_tracing::<T, _>(|t| t);
+    crate::bindings::tracing::producer::add_to_linker_get_host(l, closure)?;
 
     Ok(())
 }
 
-impl<T: WasiTracingView> crate::bindings::tracing::producer::Host for T {
-    fn send_event(&mut self, event: Vec<u8>) -> wasmtime::Result<()> {
+impl crate::bindings::tracing::producer::Host for dyn WasiTracingView + '_  {
+    fn send_event(&mut self, event: Vec<u8>) -> Result<(), wasmtime::Error> {
         let event = serde_json::from_slice(&event).map_err(wasmtime::Error::new)?;
         self.ctx()
             .0
@@ -58,6 +50,13 @@ impl WasiTracingCtx {
     }
 }
 
-pub trait WasiTracingView: Send {
+pub trait WasiTracingView:  Send  {
     fn ctx(&mut self) -> &mut WasiTracingCtx;
 }
+
+// impl<T: ?Sized + WasiTracingView> WasiTracingView for &mut T {
+//     fn ctx(&mut self) -> &mut WasiTracingCtx {
+//         T::ctx(self)
+//     }
+
+// }
